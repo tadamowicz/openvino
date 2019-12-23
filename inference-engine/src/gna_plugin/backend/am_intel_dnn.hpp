5f46d32d569860495d23e9c2ffd7904dffecfc9b
Rodionov, Pavel <pavel.rodionov@intel.com>
2019-12-23 13:46:42 +0300

    [GNA] Remove unused code from AmIntelDNN

--- inference-engine/src/gna_plugin/backend/am_intel_dnn.hpp
+++ inference-engine/src/gna_plugin/backend/am_intel_dnn.hpp
@@ -82,100 +82,6 @@ public:
                                    true);
     }
 
-    template<class A, class B, class C, class D>
-    void InitAffineComponent(uint32_t component_index,
-                             uint32_t num_rows_in,
-                             uint32_t num_columns,
-                             uint32_t num_rows_out,
-                             uint32_t num_bytes_per_input,
-                             uint32_t num_bytes_per_output,
-                             uint32_t num_bytes_per_weight,
-                             uint32_t num_bytes_per_bias,
-                             float weight_scale_factor,
-                             float output_scale_factor,
-                             A *&ptr_inputs,
-                             B *&ptr_outputs,
-                             C *&ptr_weights,
-                             D *&ptr_biases,
-                             bool isDiag = false) {
-        InitAffineComponentPrivate(component[component_index],
-                                   num_rows_in,
-                                   num_columns,
-                                   num_rows_out,
-                                   num_bytes_per_input,
-                                   num_bytes_per_output,
-                                   num_bytes_per_weight,
-                                   num_bytes_per_bias,
-                                   weight_scale_factor,
-                                   output_scale_factor,
-                                   (void *&) ptr_inputs,
-                                   (void *&) ptr_outputs,
-                                   (void *&) ptr_weights,
-                                   (void *&) ptr_biases,
-                                   isDiag,
-                                   false);
-    }
-
-    void InitDiagonalComponent(uint32_t component_index,
-                               uint32_t num_rows_in,
-                               uint32_t num_columns,
-                               uint32_t num_rows_out,
-                               uint32_t num_bytes_per_input,
-                               uint32_t num_bytes_per_output,
-                               uint32_t num_bytes_per_weight,
-                               uint32_t num_bytes_per_bias,
-                               float weight_scale_factor,
-                               float output_scale_factor,
-                               void *ptr_inputs,
-                               void *ptr_outputs,
-                               void *ptr_weights,
-                               void *ptr_biases);
-
-    template<class A, class B, class C, class D>
-    void InitConvolutional1DComponent(uint32_t component_index,
-                                      uint32_t num_rows_in,
-                                      uint32_t num_columns_in,
-                                      uint32_t num_rows_out,
-                                      uint32_t num_columns_out,
-                                      uint32_t num_bytes_per_input,
-                                      uint32_t num_bytes_per_output,
-                                      uint32_t num_bytes_per_weight,
-                                      uint32_t num_bytes_per_bias,
-                                      uint32_t num_filters,
-                                      uint32_t num_filter_rows,
-                                      uint32_t num_filter_coefficients,
-                                      uint32_t num_feature_maps,
-                                      uint32_t num_feature_map_rows,
-                                      uint32_t num_feature_map_columns,
-                                      float weight_scale_factor,
-                                      float output_scale_factor,
-                                      A *&ptr_inputs,
-                                      B *&ptr_outputs,
-                                      C *&ptr_filters,
-                                      D *&ptr_biases) {
-        InitConvolutional1DComponentPrivate(component[component_index],
-                                            num_rows_in,
-                                            num_columns_in,
-                                            num_rows_out,
-                                            num_columns_out,
-                                            num_bytes_per_input,
-                                            num_bytes_per_output,
-                                            num_bytes_per_weight,
-                                            num_bytes_per_bias,
-                                            num_filters,
-                                            num_filter_rows,
-                                            num_filter_coefficients,
-                                            num_feature_maps,
-                                            num_feature_map_rows,
-                                            num_feature_map_columns,
-                                            weight_scale_factor,
-                                            output_scale_factor,
-                                            (void *&) ptr_inputs,
-                                            (void *&) ptr_outputs,
-                                            (void *&) ptr_filters,
-                                            (void *&) ptr_biases,
-                                            false);
-    }
 
     template<class A, class B, class C, class D>
     static void InitConvolutional1DComponent(intel_dnn_component_t &comp,
@@ -224,38 +130,6 @@ public:
     }
 
 
-    // TODO: this functions accepted component_index only used in legacy code
-    void InitMaxpoolComponent(uint32_t component_index,
-                              uint32_t num_rows_in,
-                              uint32_t num_columns_in,
-                              uint32_t num_rows_out,
-                              uint32_t num_columns_out,
-                              uint32_t num_bytes_per_input,
-                              uint32_t num_bytes_per_output,
-                              uint32_t num_pool_size,
-                              uint32_t num_pool_step,
-                              uint32_t num_pool_stride,
-                              bool do_sum_not_max,
-                              float output_scale_factor,
-                              void *ptr_inputs,
-                              void *ptr_outputs) {
-        InitMaxpoolComponentPrivate(component[component_index],
-                                    num_rows_in,
-                                    num_columns_in,
-                                    num_rows_out,
-                                    num_columns_out,
-                                    num_bytes_per_input,
-                                    num_bytes_per_output,
-                                    num_pool_size,
-                                    num_pool_step,
-                                    num_pool_stride,
-                                    do_sum_not_max,
-                                    output_scale_factor,
-                                    (void *&) ptr_inputs,
-                                    (void *&) ptr_outputs,
-                                    false);
-    }
-
     template<class A, class B>
     static void InitMaxpoolComponent(intel_dnn_component_t &cmp,
                                      uint32_t num_rows_in,
@@ -289,35 +163,6 @@ public:
     }
 
 
-    void InitPiecewiseLinearComponent(uint32_t component_index,
-                                      DnnActivation function_id,
-                                      intel_dnn_orientation_t orientation,
-                                      uint32_t num_rows,
-                                      uint32_t num_columns,
-                                      uint32_t num_bytes_per_input,
-                                      uint32_t num_bytes_per_output,
-                                      uint32_t num_segments,
-                                      float output_scale_factor,
-                                      float input_scale_factor,
-                                      void *ptr_inputs,
-                                      void *ptr_outputs,
-                                      intel_pwl_segment_t *ptr_segments) {
-        InitPiecewiseLinearComponentPrivate(component[component_index],
-                                            function_id,
-                                            orientation,
-                                            num_rows,
-                                            num_columns,
-                                            num_bytes_per_input,
-                                            num_bytes_per_output,
-                                            num_segments,
-                                            output_scale_factor,
-                                            input_scale_factor,
-                                            ptr_inputs,
-                                            ptr_outputs,
-                                            ptr_segments,
-                                            false);
-    }
-
     template<class A, class B>
     static void InitPiecewiseLinearComponent(intel_dnn_component_t &cmp,
                                              DnnActivation function_id,
@@ -349,70 +194,6 @@ public:
     }
 
 
-    void InitRecurrentComponent(uint32_t component_index,
-                                uint32_t num_rows,
-                                uint32_t num_columns_in,
-                                uint32_t num_columns_out,
-                                uint32_t num_bytes_per_input,
-                                uint32_t num_bytes_per_output,
-                                uint32_t num_vector_delay,
-                                uint32_t num_bytes_per_weight,
-                                uint32_t num_bytes_per_bias,
-                                float weight_scale_factor,
-                                float output_scale_factor,
-                                void *ptr_inputs,
-                                void *ptr_feedbacks,
-                                void *ptr_outputs,
-                                void *ptr_weights,
-                                void *ptr_biases);
-
-    void InitInterleaveComponent(uint32_t component_index,
-                                 uint32_t num_rows,
-                                 uint32_t num_columns,
-                                 uint32_t num_bytes_per_input,
-                                 uint32_t num_bytes_per_output,
-                                 float output_scale_factor,
-                                 void *ptr_inputs,
-                                 void *ptr_outputs);
-
-    void InitDeinterleaveComponent(uint32_t component_index,
-                                   uint32_t num_rows,
-                                   uint32_t num_columns,
-                                   uint32_t num_bytes_per_input,
-                                   uint32_t num_bytes_per_output,
-                                   float output_scale_factor,
-                                   void *ptr_inputs,
-                                   void *ptr_outputs);
-
-    void InitCopyComponent(uint32_t component_index,
-                           intel_dnn_orientation_t orientation,
-                           uint32_t num_rows_in,
-                           uint32_t num_columns_in,
-                           uint32_t num_rows_out,
-                           uint32_t num_columns_out,
-                           uint32_t num_bytes_per_input,
-                           uint32_t num_bytes_per_output,
-                           float output_scale_factor,
-                           uint32_t num_copy_rows,
-                           uint32_t num_copy_columns,
-                           void *ptr_inputs,
-                           void *ptr_outputs) {
-        InitCopyComponentPrivate(component[component_index],
-                                 orientation,
-                                 num_rows_in,
-                                 num_columns_in,
-                                 num_rows_out,
-                                 num_columns_out,
-                                 num_bytes_per_input,
-                                 num_bytes_per_output,
-                                 output_scale_factor,
-                                 num_copy_rows,
-                                 num_copy_columns,
-                                 ptr_inputs,
-                                 ptr_outputs,
-                                 false);
-    }
-
     template<class A, class B>
     static void InitCopyComponent(intel_dnn_component_t &cmp,
                                   intel_dnn_orientation_t orientation,
@@ -443,23 +224,8 @@ public:
                                  true);
     }
 
-    void AddComponents(uint32_t num_components_to_add);
-
-    void ClearComponent(uint32_t component_index);
-
-    void ClearState();
-
-    uint32_t CopyActiveList(std::vector<std::vector<uint32_t> > &active_list, uint32_t list_index);
 
     void Propagate();
-
-    intel_dnn_macro_operation_t MacroOperation(uint32_t component_index);
-
-    void SetMacroOperation(uint32_t component_index, intel_dnn_macro_operation_t macro_operation);
-
-    float InputScaleFactor(uint32_t component_index);
-
-    float WeightScaleFactor(uint32_t component_index);
 
     float OutputScaleFactor(uint32_t component_index) {
         return OutputScaleFactor(component[component_index]);
@@ -467,21 +233,10 @@ public:
 
     float OutputScaleFactor(intel_dnn_component_t &comp);
 
-    void SetInputScaleFactor(float scale_factor) { input_scale_factor_ = scale_factor; }
-
-    void SetOutputScaleFactor(uint32_t component_index, float scale_factor);
-
-    void PrintOutputs(uint32_t component_index);
-
-    uint32_t CompareScores(void *ptr_scores, intel_score_error_t *score_error, uint32_t num_frames);
-
     void WriteGraphWizModel(const char *filename);
 
     void WriteDnnText(const char *filename, intel_dnn_number_type_t number_type);
 
-    uint32_t MemoryRequiredToReadDnnText(const char *filename);
-
-    void ReadDnnText(const char *filename, void *ptr_memory, uint32_t num_memory_bytes, float *ptr_scale_in);
 
 #if GNA_LIB_VER == 2
     void InitGNAStruct(Gna2Model *gnaModel);
@@ -493,8 +248,6 @@ public:
     void DestroyGNAStruct(intel_nnet_type_t *ptr_nnet);
 
 #endif
-
-    void GetScaledOutput(float *ptr_output, uint32_t component_index);
 
     uint32_t *ptr_active_outputs() { return (ptr_active_outputs_); }
 
